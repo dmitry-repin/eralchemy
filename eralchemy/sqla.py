2b3d367e9c4211ee5cf0a8feae8ad4264543cfd6
Dmitry Repin <dmitry.repin@gmail.com>
2019-08-16 15:29:19 +0300

    fix: relations for SQLAlchemy

--- eralchemy/sqla.py
+++ eralchemy/sqla.py
@@ -1,28 +1,36 @@
-# -*- coding: utf-8 -*-
 """
-This class allow to transform SQLAlchemy metadata to the intermediary syntax.
+This module allows transforming SQLAlchemy metadata to intermediary syntax.
 """
+import sys
+
+from sqlalchemy.exc import CompileError
 
 from eralchemy.models import Relation, Column, Table
-import sys
-from sqlalchemy.exc import CompileError
+
 
 if sys.version_info[0] == 3:
     unicode = str
 
 
 def relation_to_intermediary(fk):
-    """Transform an SQLAlchemy ForeignKey object to it's intermediary representation. """
+    """
+    Transforms an SQLAlchemy ForeignKey object to its intermediary
+    representation.
+    """
+    parent = fk.parent
+    referred = fk.column
+
     return Relation(
-        right_col=format_name(fk.parent.table.fullname),
-        left_col=format_name(fk._column_tokens[1]),
-        right_cardinality='?',
-        left_cardinality='*',
+        left_col=format_name(parent.table.fullname),
+        right_col=format_name(referred.table.fullname),
+        left_cardinality='*' if parent.nullable else '+',
+        right_cardinality='?' if referred.nullable else '1',
     )
 
 
 def format_type(typ):
-    """ Transforms the type into a nice string representation. """
+    """ Transforms the type into a nice string representation.
+    """
     try:
         return unicode(typ)
     except CompileError:
@@ -30,12 +38,15 @@ def format_type(typ):
 
 
 def format_name(name):
-    """ Transforms the name into a nice string representation. """
+    """ Transforms the name into a nice string representation.
+    """
     return unicode(name)
 
 
 def column_to_intermediary(col, type_formatter=format_type):
-    """Transform an SQLAlchemy Column object to it's intermediary representation. """
+    """ Transforms an SQLAlchemy Column object to it's intermediary
+    representation.
+    """
     return Column(
         name=col.name,
         type=type_formatter(col.type),
@@ -44,7 +55,9 @@ def column_to_intermediary(col, type_formatter=format_type):
 
 
 def table_to_intermediary(table):
-    """Transform an SQLAlchemy Table object to it's intermediary representation. """
+    """ Transforms an SQLAlchemy Table object to it's intermediary
+    representation.
+    """
     return Table(
         name=table.fullname,
         columns=[column_to_intermediary(col) for col in table.c._data.values()]
@@ -52,25 +65,37 @@ def table_to_intermediary(table):
 
 
 def metadata_to_intermediary(metadata):
-    """ Transforms SQLAlchemy metadata to the intermediary representation. """
-    tables = [table_to_intermediary(table) for table in metadata.tables.values()]
-    relationships = [relation_to_intermediary(fk) for table in metadata.tables.values() for fk in table.foreign_keys]
+    """ Transforms SQLAlchemy metadata to the intermediary representation.
+    """
+    tables = [
+        table_to_intermediary(table) for table in metadata.tables.values()
+    ]
+    relationships = [
+        relation_to_intermediary(fk)
+        for table in metadata.tables.values()
+        for fk in table.foreign_keys
+    ]
     return tables, relationships
 
 
 def declarative_to_intermediary(base):
-    """ Transform an SQLAlchemy Declarative Base to the intermediary representation. """
+    """Transforms an SQLAlchemy Declarative Base to the intermediary
+    representation.
+    """
     return metadata_to_intermediary(base.metadata)
 
 
 def name_for_scalar_relationship(base, local_cls, referred_cls, constraint):
-    """ Overriding naming schemes. """
+    """ Overrides naming schemes.
+    """
     name = referred_cls.__name__.lower() + "_ref"
     return name
 
 
 def database_to_intermediary(database_uri, schema=None):
-    """ Introspect from the database (given the database_uri) to create the intermediary representation. """
+    """ Introspects from the database (given the database_uri) to create the
+    intermediary representation.
+    """
     from sqlalchemy.ext.automap import automap_base
     from sqlalchemy import create_engine
 
@@ -80,5 +105,9 @@ def database_to_intermediary(database_uri, schema=None):
         Base.metadata.schema = schema
 
     # reflect the tables
-    Base.prepare(engine, reflect=True, name_for_scalar_relationship=name_for_scalar_relationship)
+    Base.prepare(
+        engine,
+        reflect=True,
+        name_for_scalar_relationship=name_for_scalar_relationship
+    )
     return declarative_to_intermediary(Base)
